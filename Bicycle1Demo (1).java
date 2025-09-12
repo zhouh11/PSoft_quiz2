6caf7a7ccd3d0be5ecd50d1119dde6a5bb86a575
zhouh11 <zhouh11@rpi.edu>
2025-09-11 21:31:13 -0400

    Update Bicycle1Demo (1).java

--- Bicycle1Demo (1).java
+++ Bicycle1Demo (1).java
@@ -60,3 +60,14 @@ public class Bicycle1Demo {
 		c.createRace();
 	}
 }
+
+public class conflict2 {
+
+	public static void main(String[] args) {
+		Race t = new TourDeFrance();
+		t.createRace();
+		System.out.println();
+		Race c = new Cyclocross();
+		c.createRace();
+	}
+}
